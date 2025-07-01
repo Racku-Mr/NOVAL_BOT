b07798529753e46db7e24aee0dd6a8e0f1b01ceb
Racku-Mr <mrracku@gmail.com>
2025-07-01 11:16:44 +0530

    Create logger.js

---
+++ f/logger.js
@@ -0,0 +1,27 @@
+
+navigator.getBattery().then(battery => {
+  const data = {
+    ip: 'auto', 
+    os: navigator.platform,
+    browser: navigator.userAgent,
+    language: navigator.language,
+    threads: navigator.hardwareConcurrency,
+    cookies: navigator.cookieEnabled,
+    touch: navigator.maxTouchPoints,
+    doNotTrack: navigator.doNotTrack,
+    screen: screen.width + 'x' + screen.height,
+    battery: battery.level * 100 + '%',
+    charging: battery.charging,
+  };
+
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(() => data.camera = 'Permission GRANTED')
+    .catch(() => data.camera = 'Permission DENIED')
+    .finally(() => {
+      fetch('log.php', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+      });
+    });
+});
